13a89cca779a212f03fcdcf58e9557bf9eca9754
MatiasYacob <matiasyacob@hotmail.com>
2025-05-15 13:53:50 -0300

    servidor express creado y pagina loggin

---
+++ app/index.js
@@ -0,0 +1,23 @@
+//Import Express
+import express from 'express';
+
+//__dirname
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+//Server
+const app = express();
+
+//Port
+app.set('port', 4000);
+app.listen(app.get('port'), () => {
+    console.log(`Server Runing on port ${app.get('port')}`);
+    console.log(__dirname);
+});
+//Rutas
+app.get('/', (req, res) => { res.sendFile(__dirname + "/pages/login.html") });
+app.get('/register', (req, res) => { res.sendFile(__dirname + "/pages/register.html") });
+
+//config    
+app.use(express.static(__dirname + '/public'));
